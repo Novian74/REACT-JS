21811b84be484b1ea3114408b921945d02a30f81
Novian74 <rahmandha74@gmail.com>
2023-01-18 19:20:26 +0700

    video 1-6

--- react-js/src/pages/Kontak.js
+++ react-js/src/pages/Kontak.js
@@ -1,8 +1,14 @@
+const css = {
+    color: "red",
+    background: "yellow",
+    fontSize: 50
+}
+
 function Kontak() {
     return (
         <div className="App">
-            <h1>Kontak Mistake Shop</h1>
-            <p>Sidaorjo</p>
+            <h1 className="judul">Kontak Mistake Shop</h1>
+            <p style={css}>Sidaorjo</p>
         </div>
     );
 }
--- react-js/src/pages/Tentang.js
+++ react-js/src/pages/Tentang.js
@@ -3,7 +3,9 @@ function Tentang() {
         <div className="App">
             <h1>Tentang Mistake Shop</h1>
             <p>Isi Tentang Mistake Shop</p>
-        </div>
+            <button type="button" class="btn btn-primary">Primary</button>
+            <button type="button" class="btn btn-success">Success</button>
+        </div >
     );
 }
 
