a166fe84831eeea2ee470f88628bf5def93e8a91
Novian74 <rahmandha74@gmail.com>
2023-01-18 09:05:01 +0700

    video 1-3

---
+++ react-js/src/pages/Kontak.js
@@ -0,0 +1,10 @@
+function Kontak() {
+    return (
+        <div className="App">
+            <h1>Kontak Mistake Shop</h1>
+            <p>Sidaorjo</p>
+        </div>
+    );
+}
+
+export default Kontak;
---
+++ react-js/src/pages/Tentang.js
@@ -0,0 +1,10 @@
+function Tentang() {
+    return (
+        <div className="App">
+            <h1>Tentang Mistake Shop</h1>
+            <p>Isi Tentang Mistake Shop</p>
+        </div>
+    );
+}
+
+export default Tentang;
